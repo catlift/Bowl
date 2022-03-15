46a06b845a35b75ebe9f3a76589df3d4061cdb66
liftACat <2230108070@qq.com>
2022-03-15 10:26:34 +0800

    c

--- src/settings.3aba9.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/gameScene.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"d7fa5",main:"2d373"}};
